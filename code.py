ae5fb991ed513e581ef05f750094172ff2ce2885
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-16 19:35:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,80 @@
+# ваш код
+from turtle import *
+from random import randint
+from time import sleep
+
+ht()
+
+
+def create_t(x, y, h, col):
+    t = Turtle()
+    t.color(col)
+    t.shape("turtle")
+    t.setheading(h)
+    t.pu()
+    t.goto(x, y)
+    t.pd()
+    return t
+
+
+# 1.Ігрове поле
+screen = getscreen()
+screen.bgcolor("yellow")
+
+# 2.Лейбли-лічильник
+click = create_t(200, 100, 0, "blue")
+click.count = 0
+click.ht()
+click.write(f"Click: {click.count}", font=("Arial", 24))
+
+runner = create_t(200, 150, 0, "red")
+runner.count = 0
+runner.ht()
+runner.write(f"Runner: {runner.count}", font=("Arial", 24))
+
+# 3.Черепашки-втікачки
+# 3.1 створити черепашок
+t1 = create_t(0, 0, 0, "pink")
+t2 = create_t(0, 0, 90, "violet")
+t3 = create_t(0, 0, 180, "lightblue")
+t4 = create_t(0, 0, 270, "gold")
+
+
+# 3.2 функції-оброюники кліку
+def click1(x, y):
+    t1.lt(randint(30, 200))
+    click.count += 1
+    click.clear()
+    click.write(f"Click: {click.count}", font=("Arial", 24))
+
+
+def click2(x, y):
+    t2.lt(randint(30, 200))
+    click.count += 1
+    click.clear()
+    click.write(f"Click: {click.count}", font=("Arial", 24))
+
+
+def click3(x, y):
+    t3.lt(randint(30, 200))
+    click.count += 1
+    click.clear()
+    click.write(f"Click: {click.count}", font=("Arial", 24))
+
+
+def click4(x, y):
+    t4.lt(randint(30, 200))
+    click.count += 1
+    click.clear()
+    click.write(f"Click: {click.count}", font=("Arial", 24))
+
+
+# 3.3 підписка на подію клік по черепашках
+t1.onclick(click1)
+t2.onclick(click2)
+t3.onclick(click3)
+t4.onclick(click4)
+
 # 4. Ігровий цикл
 while click.count < 20 and runner.count < 4:
     if abs(t1.xcor()) < 200 and abs(t1.ycor()) < 200:
@@ -38,3 +115,5 @@ if runner.count >= 4:
     runner.pu()
     runner.goto(0, 0)
     runner.write("You lose", font=("Arial", 40))
+
+
