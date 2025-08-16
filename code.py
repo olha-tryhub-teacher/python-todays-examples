067ae6d36a7c857766e79de65dd0bee5c7ca3d06
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-16 19:18:50 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,11 @@
-# 1.Ігрове поле
-screen = getscreen()
-screen.bgcolor("yellow")
+# 2.Лейбли-лічильник
+click = create_t(200, 100, 0, "blue")
+click.count = 0
+click.ht()
+click.write(f"Click: {click.count}", font=("Arial", 24))
+
+# те саме тільки раннер
+runner = create_t(200, 150, 0, "red")
+runner.count = 0
+runner.ht()
+runner.write(f"Runner: {runner.count}", font=("Arial", 24))
