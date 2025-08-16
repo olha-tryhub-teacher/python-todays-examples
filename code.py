6cbff1eaee5d598b924fb4a043567c1cc4eaf059
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-16 19:16:45 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,3 @@
-# ваш код
-from turtle import *
-from random import randint
-
-ht()
-
-
-def create_t(x, y, h, col):
-    t = Turtle()
-    t.color(col)
-    t.shape("turtle")
-    t.setheading(h)
-    t.pu()
-    t.goto(x, y)
-    t.pd()
-    return t
+# 1.Ігрове поле
+screen = getscreen()
+screen.bgcolor("yellow")
