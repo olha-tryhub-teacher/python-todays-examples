fea3e22c48d4844c621e88d72386988b140cf2a5
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-16 19:22:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,5 @@
-# 2.Лейбли-лічильник
-click = create_t(200, 100, 0, "blue")
-click.count = 0
-click.ht()
-click.write(f"Click: {click.count}", font=("Arial", 24))
-
-# те саме тільки раннер
-runner = create_t(200, 150, 0, "red")
-runner.count = 0
-runner.ht()
-runner.write(f"Runner: {runner.count}", font=("Arial", 24))
+def click1(x, y):
+    t1.lt(randint(30, 200))
+    click.count += 1
+    click.clear()
+    click.write(f"Click: {click.count}", font=("Arial", 24))
