02cecd63b63740ff39068c37ab9009a96c4a698b
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-23 19:22:33 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,60 @@
-# 2.2 Функція що оновлює напис
-def update_count(label, txt):
-    label.count += 1
-    label.clear()
-    label.write(f"{txt} : {label.count}", font=("Arial", 16))
+
+# 4.2 Функція появи нового яблука
+def spawn_a():
+    a = create_t(randint(-130, 130), 150, "circle", choice(colors))
+    apples.append(a)
 
 
-# 3.Платформа-гравець
-# 3.1 Створити гравця
-plt = create_t(0, -130, "square", "violet")
+# 4.3 Функця руху яблука
+def move(a):
+    a.fd(5)
 
 
-# 3.2 Функції руху праворчу-ліворуч
-def move_l():
-    plt.setheading(180)
-    plt.fd(10)
+# 4.4 Функція перевірки яблука, що впало
+def check_miss(a):
+    if a.ycor() <= - 150:
+        a.ht()
+        apples.remove(a)
+        update_count(miss, "Miss")
 
 
-def move_r():
-    plt.setheading(0)
-    plt.fd(10)
+# 4.5 Функція спійманого яблука
+def check_catch(a):
+    x, y = plt.xcor(), plt.ycor()
+    if a.distance(x, y) <= 10:
+        a.ht()
+        apples.remove(a)
+        update_count(catch, "Catch")
+
+
+# 5. Функція перевірка виграшу/програшу
+def check_end_game():
+    if miss.count >= 3:
+        miss.goto(0, 0)
+        miss.write("You lose", font=("Arial", 14))
+        return True
+    if catch.count >= 10:
+        catch.goto(0, 0)
+        catch.write("You lose", font=("Arial", 14))
+        return True
+    return False
+
+
+# 6. Функція гри
+def game():
+    if randint(1, 30) == 3:
+        spawn_a()
+    for a in apples:
+        move(a)
+        check_miss(a)
+        check_catch(a)
+    end = check_end_game()
+    if not end:
+        screen.ontimer(game, 50)
+
+
+# Запускаємо все
+update_count(miss, "Miss")
+update_count(catch, "Catch")
+spawn_a()
+game()
