dba6fab476a6a202f56a0c7bfa8c47af8541f45d
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-23 19:15:33 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,8 @@
-from turtle import *
-from random import randint, choice
-
-ht()
-
-
-def create_t(x, y, sh, col):
-    t = Turtle()
-    t.speed(0)
-    t.pu()
-    t.color(col)
-    t.shape(sh)
-    t.setheading(270)
-    t.goto(x, y)
-    return t
-
-
-screen = getscreen()
+# 2. Лейбли - лічильники
+# 2.1 Створити лейбли
+miss = create_t(-150, 160, "turtle", "red")
+miss.count = -1
+miss.ht()
+catch = create_t(70, 160, "turtle", "green")
+catch.count = -1
+catch.ht()
