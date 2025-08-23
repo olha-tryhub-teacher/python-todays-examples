dc9358a393f1d84e09956394f425895c9bf41715
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-23 19:16:47 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,21 @@
-# 2. Лейбли - лічильники
-# 2.1 Створити лейбли
-miss = create_t(-150, 160, "turtle", "red")
-miss.count = -1
-miss.ht()
-catch = create_t(70, 160, "turtle", "green")
-catch.count = -1
-catch.ht()
+# 2.2 Функція що оновлює напис
+def update_count(label, txt):
+    label.count += 1
+    label.clear()
+    label.write(f"{txt} : {label.count}", font=("Arial", 16))
+
+
+# 3.Платформа-гравець
+# 3.1 Створити гравця
+plt = create_t(0, -130, "square", "violet")
+
+
+# 3.2 Функції руху праворчу-ліворуч
+def move_l():
+    plt.setheading(180)
+    plt.fd(10)
+
+
+def move_r():
+    plt.setheading(0)
+    plt.fd(10)
