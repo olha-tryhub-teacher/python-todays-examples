878998aa1e316d824fe77135ecd5b3154877fd9f
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-30 18:33:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,29 +1,14 @@
 from turtle import *
-from time import sleep
-from random import randint, choice, random
+from random import randint
+
+hideturtle()
 
 
-def create_t(x, y, form, col):
+def create_t(x, y, sh, col):
     t = Turtle()
+    t.shape(sh)
+    t.speed(0)
+    t.color(col)
     t.penup()
     t.goto(x, y)
-    t.shape(form)
-    t.color(col)
     return t
-
-
-t_list = []
-forms = ['triangle', 'square']
-colors = ['red', 'yellow', 'green', 'blue']
-
-for i in range(20):
-    t1 = create_t(randint(-200, 200), randint(-200, 200), choice(forms), choice(colors))
-    t_list.append(t1)
-
-for i in range(50):
-    for t in t_list:
-        t.hideturtle()
-    sleep(randint(1,5)/10)
-    for t in t_list:
-        t.showturtle()
-    sleep(randint(1,5)/10)
