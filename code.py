8caef06f9f77bdb59af0de9860ca44f4468a80e5
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-08-30 18:37:13 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,16 @@
+# 1.Функція - малюємо ігрове поле
+def draw_field():
+    t = create_t(-150, -100, "turtle", "gold")
+    t.hideturtle()
+    t.width(10)
+    t.pendown()
+    for _ in range(2):
+        t.fd(300)
+        t.lt(90)
+        t.fd(200)
+        t.lt(90)
+
+
 # 2. Функція - створити об'єкт черепашка(координати та колір). Метод - перетягувати
 def create_turtle(x, y, col):
     t = create_t(x, y, "circle", col)
