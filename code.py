5f1280f93cadb8a872c1a5fcd7d47379c452ee02
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-01 19:20:54 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,38 +1,42 @@
-from pydantic import BaseModel
-from typing import List, Optional
+from sqlalchemy.orm import Session
+import models, schemas
 
 
-# ----- Book Schemas -----
+# ----- Author -----
 
-class BookBase(BaseModel):
-    name: str
-
-
-class BookCreate(BookBase):
-    author_id: int
+def get_author(db: Session, author_id: int):
+    return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-class Book(BookBase):
-    id: int
-    author_id: int
-
-    class Config:
-        orm_mode = True   # дозволяє повертати дані з SQLAlchemy-моделей
+def get_author_by_name(db: Session, name: str):
+    return db.query(models.Author).filter(models.Author.name == name).first()
 
 
-# ----- Author Schemas -----
-
-class AuthorBase(BaseModel):
-    name: str
+def get_authors(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Author).offset(skip).limit(limit).all()
 
 
-class AuthorCreate(AuthorBase):
-    pass
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.Author(name=author.name)
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
 
 
-class Author(AuthorBase):
-    id: int
-    books: List[Book] = []   # Автор може мати список книжок
+# ----- Book -----
 
-    class Config:
-        orm_mode = True
+def get_book(db: Session, book_id: int):
+    return db.query(models.Book).filter(models.Book.id == book_id).first()
+
+
+def get_books(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Book).offset(skip).limit(limit).all()
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Book(name=book.name, author_id=book.author_id)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
