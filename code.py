4782423e0ebf44c739aa8d106053b0be575b7cdc
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-01 18:46:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,23 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./library.db"
+from database import Base
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    author_id = Column(Integer, ForeignKey("authors.id"))
+
+    author = relationship("Author", back_populates="books")
