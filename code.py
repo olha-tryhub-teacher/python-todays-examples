db86fa69668038dcf31d395adc391c3e6b76593c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-06 13:52:45 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,126 +1,74 @@
-from pygame import *
-import socket
-import json
-from threading import Thread
-
-# ---ПУГАМЕ НАЛАШТУВАННЯ ---
-WIDTH, HEIGHT = 800, 600
-init()
-screen = display.set_mode((WIDTH, HEIGHT))
-clock = time.Clock()
-display.set_caption("Пінг-Понг")
-
-
-# ---СЕРВЕР ---
-def connect_to_server():
-    while True:
-        try:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect(('localhost', 8080))  # ---- Підключення до сервера
-            buffer = ""
-            game_state = {}
-            my_id = int(client.recv(24).decode())
-            return my_id, game_state, buffer, client
-        except:
-            pass
-
-
-def receive():
-    global buffer, game_state, game_over
-    while not game_over:
-        try:
-            data = client.recv(1024).decode()
-            buffer += data
-            while "\n" in buffer:
-                packet, buffer = buffer.split("\n", 1)
-                if packet.strip():
-                    game_state = json.loads(packet)
-        except:
-            game_state["winner"] = -1
-            break
-
-
-# --- ШРИФТИ ---
-font_win = font.Font(None, 72)
-font_main = font.Font(None, 36)
-# --- ЗОБРАЖЕННЯ ----
-# game_bg = image.load("images/football-field.webp")
-# game_bg = transform.scale(game_bg, (WIDTH, HEIGHT))
-
-# --- ЗВУКИ ---
-
-# --- ГРА ---
-game_over = False
-winner = None
-you_winner = None
-my_id, game_state, buffer, client = connect_to_server()
-Thread(target=receive, daemon=True).start()
+# 2
 while True:
-    for e in event.get():
-        if e.type == QUIT:
-            exit()
+    try:
+        players_amount = int(input('Введіть кількість гравців: '))
+        bonus_amount = int(input('Введіть кількість бонусів: '))
+        break
+    except:
+        print('Помилка! Кількість повинна бути цілим числом')
 
-    if "countdown" in game_state and game_state["countdown"] > 0:
-        screen.fill((0, 0, 0))  # це можна закоментувати
-        # todo: тут додати малювання фону для екрану, де рахує 3... 2... 1... до початку гри
-        countdown_text = font.Font(None, 72).render(str(game_state["countdown"]), True, (255, 255, 255))
-        screen.blit(countdown_text, (WIDTH // 2 - 20, HEIGHT // 2 - 30))
-        display.update()
-        continue  # Не малюємо гру до завершення відліку
+# ⬇️⬇️⬇️ Обчислення бонусу з обробкою ділення на 0
+try:
+    bonus = bonus_amount / players_amount
+    print(f"Кожен гравець отримає {bonus} бонусів")
+except:
+    print("Помилка поділу! Можливо, ніхто не прийшов на турнір?")
 
-    if "winner" in game_state and game_state["winner"] is not None:
-        screen.fill((20, 20, 20))
+# 3
+print('Як називається найвища гора світу?')
+print('1 - Ельбрус, 2 - Мауна-Лоа, 3 - Еверест, 4 - Деналі')
 
-        if you_winner is None:  # Встановлюємо тільки один раз
-            if game_state["winner"] == my_id:
-                you_winner = True
-            else:
-                you_winner = False
+# тут пиши код ⬇️⬇️⬇️
+while True:
+    answer = input()
+    try:
+        answer = int(answer)
+        break
+    except:
+        print("Помилка! Введіть номер правильної відповіді")
+# тут пиши код ⬆️⬆️⬆️
 
-        if you_winner:
-            text = "Ти переміг!"
+if answer == 3:
+    print('Абсолютно вірно!')
+else:
+    print('Ні. Еверест, 8848 метрів')
+
+# 4
+print('Як розшифровується ВООЗ?')
+print('''1-Всесвітня організація охорони здоров'я''')
+print('2-Всеукраїнська організація захисту')
+print('3-Всеукраїнська особлива здравниця')
+print('4-Володіння особливого значення')
+
+# Допиши програму
+while True:
+    answer = input()
+    try:
+        answer = int(answer)
+        break
+    except:
+        print("Помилка! Введіть номер правильної відповіді")
+
+if answer == 1:
+    print("Абсолютно вірно!")
+else:
+    print("Ні. ВООЗ - це Всесвітня організація охорони здоров'я")
+
+# 5
+from random import *
+
+random_number = randint(1, 10)
+
+while True:
+    answer = input("Вгадай число від 1 до 10:")
+    try:
+        answer = int(answer)
+        if answer < 1 or answer > 10:
+            print("Число має бути від 1 до 10.")
+        elif answer != random_number:
+            print("Спробуй ще раз")
         else:
-            text = "Пощастить наступним разом!"
-
-        win_text = font_win.render(text, True, (255, 215, 0))
-        text_rect = win_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-        screen.blit(win_text, text_rect)
-
-        text = font_win.render('К - рестарт', True, (255, 215, 0))
-        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 120))
-        # todo: тут малюємо фон для кінця гри (тіпа гейм овер)
-        screen.blit(text, text_rect)
-
-        display.update()
-        continue  # Блокує гру після перемоги
-
-    if game_state:
-        # screen.fill((30, 30, 30))
-        screen.blit(game_bg, (0, 0)) # todo: ось приклад малювання фону гри (у мене було футбольне поле)
-        draw.rect(screen, (0, 255, 0), (20, game_state['paddles']['0'], 20, 100))
-        draw.rect(screen, (255, 0, 255), (WIDTH - 40, game_state['paddles']['1'], 20, 100))
-        draw.circle(screen, (255, 255, 255), (game_state['ball']['x'], game_state['ball']['y']), 10)
-        score_text = font_main.render(f"{game_state['scores'][0]} : {game_state['scores'][1]}", True, (255, 255, 255))
-        screen.blit(score_text, (WIDTH // 2 - 25, 20))
-
-        if game_state['sound_event']:
-            if game_state['sound_event'] == 'wall_hit':
-                # звук відбиття м'ячика від стін
-                pass
-            if game_state['sound_event'] == 'platform_hit':
-                # звук відбиття м'ячика від платформи
-                pass
-
-    else:
-        # todo: тут можна додати малювання фону для очікування другого гравця
-        wating_text = font_main.render(f"Очікування гравців...", True, (255, 255, 255))
-        screen.blit(wating_text, (WIDTH // 2 - 25, 20))
-
-    display.update()
-    clock.tick(60)
-
-    keys = key.get_pressed()
-    if keys[K_w]:
-        client.send(b"UP")
-    elif keys[K_s]:
-        client.send(b"DOWN")
+            print("Вітаю! Ви вгадали!")
+            break
+    except:
+        print("Помилка! Введіть число.")
