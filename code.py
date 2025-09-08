45e32d7d8cb6f50c8166d6090f3356d9e63ff8c5
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-08 19:26:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,7 @@
-# ----- User Schemas -----
-
-# Базова схема користувача, містить тільки логін
-class UserBase(BaseModel):
-    login: str
-
-# Схема для роботи з БД, містить логін і пароль
-class UserDB(UserBase):
-    password: str
-
-# Схема для створення нового користувача через API
-class UserCreate(UserBase):
-    password: str
-
-# Схема для відповіді API, включає id користувача
-class User(UserBase):
-    id: int
-    # Дозволяє створювати Pydantic-модель із об'єктів ORM
-    class Config:
-        from_attributes = True
+# new imports ⬇️⬇️⬇️
+from typing import Annotated
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+# new imports ⬆️⬆️⬆️
