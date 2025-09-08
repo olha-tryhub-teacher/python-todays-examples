dcbb0051e4fe4d7a2c13aa35f03ec2943ac50a7c
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-08 19:24:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,20 @@
-class User(Base):
-    __tablename__ = "users"
+# ----- User Schemas -----
 
-    id = Column(Integer, primary_key=True, index=True)
-    login = Column(String, unique=True, index=True)
-    password = Column(String)
-    salt = Column(String, nullable=True)
+# Базова схема користувача, містить тільки логін
+class UserBase(BaseModel):
+    login: str
+
+# Схема для роботи з БД, містить логін і пароль
+class UserDB(UserBase):
+    password: str
+
+# Схема для створення нового користувача через API
+class UserCreate(UserBase):
+    password: str
+
+# Схема для відповіді API, включає id користувача
+class User(UserBase):
+    id: int
+    # Дозволяє створювати Pydantic-модель із об'єктів ORM
+    class Config:
+        from_attributes = True
