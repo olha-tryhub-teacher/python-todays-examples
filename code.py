ae4623e8c2917c9a36e41d44610fb0a4cfb0b94e
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-13 15:25:49 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,14 @@
-if command == 'NAME':
-   connection.send(f"Твоє ім'я: {client_name}".encode())
-elif command == 'EXIT':
-   connection.send(f"Бувай {client_name}".encode())
-   connection.close()
-else:
-   connection.send('Такої команди не існує'.encode())
-server_socket.close()
+from socket import *
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+
+name = input("Для підключення на сервер введіть своє ім'я: ")
+client_socket.send(name.encode())
+print(client_socket.recv(1024).decode())
+command = input('Введіть команду (NAME, EXIT): ')
+client_socket.send(command.encode())
+print(client_socket.recv(1024).decode())
+
+client_socket.close()
+
