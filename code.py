7018825fbee82e88d20cad7211cd4cc0401eab98
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-13 15:26:31 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,3 +12,17 @@ print(client_socket.recv(1024).decode())
 
 client_socket.close()
 
+
+
+
+# ДЛЯ ВАНІ
+if command == 'NAME':
+   connection.send(f"Твоє ім'я: {client_name}".encode())
+elif command == 'EXIT':
+   connection.send(f"Бувай {client_name}".encode())
+   connection.close()
+else:
+   connection.send('Такої команди не існує'.encode())
+server_socket.close()
+
+
