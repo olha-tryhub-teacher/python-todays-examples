63cacbcb8cc4f88077917af3120e1291621749e2
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-15 19:23:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,96 +1,36 @@
-# Загальний план будинку (список усіх кімнат)
-plan_house = [
-    data_room1, data_room2,
-    data_living, data_corridor,
-    data_toilet, data_bath,
-    data_kitchen
-]
-
-from turtle import *
+@app.get("/home", response_class=HTMLResponse)
+def ui_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-class Room:
-    def __init__(self, x, y, w, h, bg_color, name):
-        """Ініціалізація кімнати"""
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.bg_color = bg_color
-        self.name = name
-
-        # Окремий Turtle для кожної кімнати
-        self.t = Turtle()
-        self.t.speed(0)
-        self.t.hideturtle()
-        self.t.width(10)
-        self.t.color("purple", self.bg_color)  # Колір рамки і заповнення
-
-    def go_to(self, x, y):
-        """Переміщення без малювання"""
-        self.t.penup()
-        self.t.goto(x, y)
-        self.t.pendown()
-
-    def draw(self):
-        """Малює кімнату з підписом"""
-        self.go_to(self.x, self.y)
-
-        # Малювання прямокутника
-        self.t.begin_fill()
-        for i in range(2):
-            self.t.forward(self.w)
-            self.t.left(90)
-            self.t.forward(self.h)
-            self.t.left(90)
-        self.t.end_fill()
-
-        # Запис назви кімнати
-        self.t.color("black")
-        if self.w < self.h:  # Якщо кімната вертикальна — пишемо по вертикалі
-            y_char = self.y + self.h - 45
-            for char in self.name:
-                self.go_to(self.x + 15, y_char)
-                self.t.write(char, font=("Arial", 14))
-                y_char -= 20
-        else:  # Інакше пишемо по горизонталі
-            self.go_to(self.x + 5, self.y + self.h // 3)
-            self.t.write(self.name, font=("Arial", 14))
+@app.get("/home/authors", response_class=HTMLResponse)
+def ui_authors(request: Request, db: Session = Depends(get_db)):
+    authors = crud.get_authors(db)
+    return templates.TemplateResponse( #один рядок
+        "authors.html", {"request": request, "authors": authors})
 
 
-class House:
-    def __init__(self, rooms_data_list):
-        """Створює всі кімнати за планом"""
-        self.rooms = []
-        for room in rooms_data_list:
-            r = Room(room[0], room[1], room[2], room[3], room[4], room[5])
-            self.rooms.append(r)
-
-        # Окремий Turtle для контуру будинку
-        self.h = Turtle()
-        self.h.speed(0)
-
-    def skyline(self):
-        """Малює зовнішній контур будинку"""
-        self.h.width(15)
-        self.h.color("purple")
-        self.h.hideturtle()
-        self.h.penup()
-        self.h.goto(-100, -50)
-        self.h.pendown()
-        for i in range(2):
-            self.h.forward(150 + 80)
-            self.h.left(90)
-            self.h.forward(200)
-            self.h.left(90)
-
-    def draw(self):
-        """Малює всі кімнати та контур"""
-        for room in self.rooms:
-            room.draw()
-        self.skyline()
 
 
-# Створюємо будинок і малюємо його
-house = House(plan_house)
-house.draw()
+
+@app.get("/home/books", response_class=HTMLResponse)
+def ui_books(request: Request, db: Session = Depends(get_db)):
+    books = crud.get_books(db)
+    return templates.TemplateResponse("books.html", {"request": request, "books": books})
+
+
+@app.get("/home/authors/{author_id}",  response_class=HTMLResponse)
+def ui_single_author(author_id: int, request: Request, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db, author_id=author_id)
+    if not db_author:
+        raise HTTPException(status_code=404, detail="Автор не знайдений")
+    # отримуємо книжки саме цього автора
+    books = db_author.books
+    return templates.TemplateResponse(
+        "author.html",
+        {
+            "request": request,
+            "author": db_author,
+            "books": books
+        }
+    )
