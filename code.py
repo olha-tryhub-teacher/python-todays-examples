b8a92cce9e3e20d40cddbcb00936ebee84824776
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-18 18:22:39 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,35 +1,81 @@
-from random import randint, choice  # randint - випадкове число, choice - випадковий елемент зі списку
-from turtle import *  # імпортуємо бібліотеку turtle для графіки
+from turtle import *
+from random import randint
 
-screen = Screen()  # створюємо екран для малювання
-ball = Turtle()  # створюємо "черепашку", яка буде кулькою
-ball.shape("circle")  # задаємо форму - коло
-ball.speed(0)  # швидкість малювання (0 - максимально швидко)
-
-# список кольорів для кульки
-colors = ["red", "blue", "green", "yellow", "orange", "pink"]
-
-ball.setheading(270)  # напрямок руху кульки (270 градусів = вниз)
+hideturtle()
 
 
-def start_ball():
-    """Функція створює нову кульку у випадковому місці вгорі"""
-    ball.penup()  # підняти "перо", щоб не малювало лінії
-    ball.goto(randint(-300, 300), 180)  # ставимо кульку у випадкову позицію по x, завжди вгорі (y=180)
-    ball.color(choice(colors))  # випадковий колір кульки
-    ball.pendown()  # опустити "перо", якщо потрібно малювати
+def create_t(x, y, sh, col):
+    t = Turtle()
+    t.shape(sh)
+    t.speed(0)
+    t.color(col)
+    t.penup()
+    t.goto(x, y)
+    return t
 
 
-def move_ball():
-    """Функція рухає кульку вниз і перевіряє, чи не вийшла вона за нижню межу"""
-    ball.forward(10)  # рух вперед (у нашому випадку вниз, бо heading=270)
-    if ball.ycor() < -180:  # якщо кулька дійшла до низу екрана
-        start_ball()  # запускаємо її знову зверху
-    screen.ontimer(move_ball, 10)  # 0.01sec                 # повторюємо функцію через 100 мс (анімація)
+# 1.Функція - малюємо ігрове поле
+def draw_field():
+    t = create_t(-150, -100, "turtle", "gold")
+    t.hideturtle()
+    t.width(10)
+    t.pendown()
+    for _ in range(2):
+        t.fd(300)
+        t.lt(90)
+        t.fd(200)
+        t.lt(90)
 
 
-# запускаємо гру
-start_ball()  # створюємо першу кульку
-move_ball()  # запускаємо її рух
+# 2. Функція - створити об'єкт черепашка(координати та колір). Метод - перетягувати
+def create_turtle(x, y, col):
+    t = create_t(x, y, "circle", col)
+
+    def on_drag(x, y):
+        t.goto(x, y)
+        check_turtle()
+
+    t.ondrag(on_drag)
+    return t
+
+
+# 3. створити 4 об'єкта черепашки - список
+turtle_list = []
+colors = ["red", "blue", "purple", "orange"]
+for col in colors:
+    t = create_turtle(randint(-200, 200), -150, col)
+    turtle_list.append(t)
+
+
+# 4. Функція - створити напис-позначку, метод - оновлювати напис
+def create_label(x, y):
+    t = create_t(x, y, "turtle", "violet")
+    t.hideturtle()
+
+    def write_t(count):
+        t.clear()
+        t.write(f"In rect {count} turtle", font=("Arial", 16))
+
+    t.write_t = write_t
+    return t
+
+
+# 5. Створити напис-позначку
+label = create_label(-150, -125)
+
+
+# 6.Функція - перевірити кількість черпашок у прямокутнику
+def check_turtle():
+    count = 0
+    for t in turtle_list:
+        if abs(t.xcor()) < 150 and abs(t.ycor()) < 100:
+            count += 1
+
+    label.write_t(count)
+
+
+# 7. Намалювати ігрове поле, перевірити де черепашки піся спавну
+draw_field()
+check_turtle()
 
 done()
