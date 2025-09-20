66333c6c6217206d51071722d979896f877795d2
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-09-20 13:49:50 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,52 +1,35 @@
-class Product:
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+# client
 
-    def __str__(self):
-        return f"{self.name}, Ціна: {self.price}, Кількість: {self.quantity}"
+from socket import *
 
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
 
-class Store:
-    def __init__(self):
-        self.products = []
+name = input("Для підключення на сервер введіть своє ім'я: ")
+client_socket.send(name.encode())
+print(client_socket.recv(1024).decode())
+command = input('Введіть команду (NAME, EXIT): ')
+client_socket.send(command.encode())
+print(client_socket.recv(1024).decode())
 
-    def add_product(self, product):
-        self.products.append(product)
-
-    def generate_product_list(self):
-        for product in self.products:
-            yield product
+client_socket.close()
 
 
-def generate_product_page(product_generator, limit):
-    count = 0
-    for product in product_generator:
-        if count < limit:
-            print(product)
-            count += 1
-        if count == limit:
-            break
-
-
-# Створення магазину і додавання товарів
-store = Store()
-
-store.add_product(Product("Ліжко", 500, 5))
-store.add_product(Product("Стіл", 250, 3))
-store.add_product(Product("Стілець", 50, 10))
-store.add_product(Product("Шафа", 350, 4))
-store.add_product(Product("Диван", 800, 2))
-store.add_product(Product("Комод", 150, 6))
-store.add_product(Product("Столик", 100, 8))
-store.add_product(Product("Крісло", 75, 12))
-store.add_product(Product("Тумба", 55, 5))
-store.add_product(Product("Полиця", 200, 7))
-
-
-# Генератор для виведення товарів на сторінку сайту
-products_generated = store.generate_product_list()
-
-while input() == 'c':
-    generate_product_page(products_generated, limit=3)
+# server
+from socket import *
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(('localhost', 12345))
+server_socket.listen(1)
+connection, address = server_socket.accept()
+client_name = connection.recv(1024).decode()
+print(f"Користувач {client_name} під'єднався!")
+connection.send(f"Вітаю {client_name} на сервері".encode())
+command = connection.recv(1024).decode()
+if command == 'NAME':
+   connection.send(f"Твоє ім'я: {client_name}".encode())
+elif command == 'EXIT':
+   connection.send(f"Бувай {client_name}".encode())
+   connection.close()
+else:
+   connection.send('Такої команди не існує'.encode())
+server_socket.close()
