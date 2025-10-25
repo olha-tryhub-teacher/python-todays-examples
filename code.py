7cac113c35cf6cc28c6fa19a91c1bf3f76145f29
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-10-25 19:04:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -13,40 +13,15 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 
 
-class MyRect():
-    def __init__(self, x, y):
-        self.rect = pygame.Rect(x, y, 200, 50)
-        # створення активної та пасивної поверхней
-        # створення поверхонь
-        self.normal_surf = pygame.Surface(
-            (self.rect.width, self.rect.height))
-        self.normal_surf.fill(BLUE)
-
-        self.hover_surf = pygame.Surface(
-            (self.rect.width, self.rect.height))
-        self.hover_surf.fill(RED)
-        
-        self.current_surf = self.normal_surf
-        
-    def update(self):
-        # перевірка, чи курсор мишки над прямокутником
-        x, y = pygame.mouse.get_pos()
-        if self.rect.collidepoint(x, y):
-            self.current_surf = self.hover_surf
-        else:
-            self.current_surf = self.normal_surf
-            
-
-    def draw(self, surface):
-        # відмальовка потрібної поверхні
-        surface.blit(self.current_surf, (self.rect.x, self.rect.y))
-
-
-#######################################    
-# об'єкт прямокутника, потрібно його створити
-r = MyRect(100, 100)
-running = True
 clock = pygame.time.Clock()
+running = True
+
+# створюємо шрифт
+font = pygame.font.Font(None, 48)
+font.set_italic(True)
+font.set_bold(True)
+# створення поверхні із текстом
+text_image = font.render("Hello, world!", True, YELLOW)
 
 
 while running:
@@ -56,10 +31,10 @@ while running:
             running = False
 
     # заливка екрана кольором
-    screen.fill(YELLOW)
-    # оновлення та відмальовка
-    r.update()
-    r.draw(screen)
+    screen.fill(BLUE)
+
+    # відмальовка поверхні з текстом
+    screen.blit(text_image, (150, 150))
 
     # оновлення дисплея
     pygame.display.flip()
