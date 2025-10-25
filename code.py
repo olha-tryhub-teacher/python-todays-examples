a2e3c9dc95c35e03b40a5784e9a54d4233f83161
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-10-25 15:53:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,12 @@
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 400
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREY = (200, 200, 200)
+import pygame
+from settings import WHITE, GREY, BLACK
 
-# Відповідність клавіш і нот
-KEYS = {
-    "1": "c6.mp3",
-    "2": "d6.mp3",
-    "3": "e6.mp3",
-    "4": "f6.mp3",
-    "5": "g6.mp3",
-    "6": "a6.mp3",
-    "7": "b6.mp3",
-}
+def draw_key_effect(screen, rect, is_pressed=False):
+    """Малює клавішу з ефектом"""
+    base_color = (220, 220, 220) if not is_pressed else (170, 220, 255)  # блакитний відтінок при натисканні
+    border_color = BLACK
+
+    # фон клавіші
+    pygame.draw.rect(screen, base_color, rect, border_radius=8)
+    # рамка
+    pygame.draw.rect(screen, border_color, rect, 2, border_radius=8)
