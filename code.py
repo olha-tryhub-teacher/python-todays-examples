04b8aaf2146bc8010f813c87e564608eba438eeb
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2025-11-08 13:50:16 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,29 @@
-    def is_clicked(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if self.rect.collidepoint(event.pos):
-                return True
-        return False
+from customtkinter import *
+
+name = "Оля"
+
+
+class MainWindow(CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x300")
+        # main
+        self.username = name
+        self.name_lbl = CTkLabel(self, text="Привіт, " + self.username)
+        self.name_lbl.pack()
+        self.chat_field = CTkScrollableFrame(self)
+        self.chat_field.pack(fill="both", expand=True)
+
+        self.bottom_row = CTkFrame(self)
+        self.bottom_row.pack(fill="x")
+
+        self.message_entry = CTkEntry(self.bottom_row,
+                                      placeholder_text="Введіть повідомлення:",
+                                      height=40)
+        self.message_entry.pack(fill="x", side="left", expand=True)
+        self.send_button = CTkButton(self.bottom_row, text=">", width=50, height=40)
+        self.send_button.pack(side="left")
+
+
+main_win = MainWindow()
+main_win.mainloop()
